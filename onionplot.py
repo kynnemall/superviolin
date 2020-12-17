af4bf510a64b1d1792965766032b5c6c79b15a67
Labuser <Labuser@MCT-DT-47.rcsi.com>
2020-12-17 15:34:34 +0000

    Zero the w vectors

--- onionplot.py
+++ onionplot.py
@@ -19,21 +19,25 @@ class onionplot:
         self.rep = 'replicate'
         self.p_x, self.w_y = [], []
         
-    def plot_distributions(self):
+    def plot_distributions(self, close = True):
         for i in range(3):
             plt.subplot(1, 3, i + 1)
             r = self.df[self.rep].unique()
             sub = self.df[self.df[self.rep] == r[i]]
-            x, y = sns.kdeplot(sub['order'], cut = 0).get_lines()[0].get_data()
+            x, y = sns.kdeplot(sub[self.y], cut = 0).get_lines()[0].get_data()
             self.p_x.append(x)
             self.w_y.append(y)
-        plt.close()
+        if close:
+            plt.close()
         self.p_x = np.array(self.p_x)
         self.w_y = np.array(self.w_y)
     
     def stack_kde_data(self):
-        # subtract min from y data to get close to the line
-        pass
+        # subtract min from y data to get close to a zero line
+        self.w_y = self.w_y - np.min(self.w_y, axis=(1), keepdims=True)
+        # create y values which stack
+        
+        # normalize max to a certain range
     
     def plot(self):
         pass
