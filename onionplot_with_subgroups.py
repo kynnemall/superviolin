6b25751dee5ed287498ce2995a38142549d27d5a
Martin Kenny <mkenny5@tcd.ie>
2020-12-21 17:32:58 +0000

    another push

--- onionplot_with_subgroups.py
+++ onionplot_with_subgroups.py
@@ -18,62 +18,78 @@ class onionplot:
         self.x = x
         self.y = y
         self.rep = rep_col
-        self.subgroups = subgroup
-        if self.subgroups:
+        self.subgroup = subgroup
+        if self.subgroup:
             self.subgroups = self.df[subgroup].unique()
         self.unique_reps = self.df[self.rep].unique()
-        self.p_x, self.w_y = [], []
-        self.get_kde_data(print_, plot_kde)
+        # make sure there's enough colours for each subgroup when instantiating
         self.colors = ['Green','Red','Blue','Pink','Purple']
+        self.get_kde_data(print_, plot_kde)
+        # dictionary of arrays for subgroup data
+        # loop through the keys and add an empty list when the replicate numbers don't match
+        # this dataset has 22 KDEs to calculate rather than 24
+        self.subgroup_dict = dict(zip(self.subgroups, [{'norm_wy' : [], 'px' : []}
+                                                        for i in self.subgroups]))
+        # self.plot_stripes()
+        self.palette = None
         
     def get_kde_data(self, print_ = False, plot = False):
-        min_cuts = []
-        max_cuts = []
-        for i in range(len(self.unique_reps)):
-            sub = self.df[self.df[self.rep] == self.unique_reps[i]]
-            min_cuts.append(sub[self.y].min())
-            max_cuts.append(sub[self.y].max())
-        min_cuts = sorted(min_cuts)
-        max_cuts = sorted(max_cuts)
-        # make linespace of points from highest_min_cut to lowest_max_cut
-        points = list(np.linspace(max(min_cuts), min(max_cuts), num = 128))
-        points = sorted(list(set(min_cuts + points + max_cuts)))   
-        for a in self.unique_reps:
-            sub = self.df[self.df[self.rep] == a][self.y]
-            kde = gaussian_kde(sub)
-            kde_points = kde.evaluate(points)
-            # zero the kde points
-            kde_points = kde_points - kde_points.min()
-            zeros_present = list(kde_points)
-            print(f"{zeros_present.count(0)} zeroes present already at index {zeros_present.index(0)}")
-            # use min and max to make the kde_points outside that dataset = 0
-            idx_min = min_cuts.index(sub.min())
-            idx_max = max_cuts.index(sub.max())
-            if idx_min > 0:
-                for p in range(idx_min):
-                    kde_points[p] = 0
-#            print(sub.min())
-#            print(points[:3])
-#            print(kde_points[:3])
-#            print('\n')
-            for idx in range(idx_max - len(max_cuts), 0):
-                if idx_max - len(max_cuts) != -1:
-                    kde_points[idx+1] = 0
-#            print(sub.max())
-#            print(points[-3:])
-#            print(kde_points[-3:])
-            self.w_y.append(kde_points)
-            self.p_x.append(points)
-#        self.check_cutting()
-        self.p_x = np.array(self.p_x)
-        self.w_y = np.array(self.w_y)
-        self.cum_wy = np.cumsum(self.w_y, axis = 0)
-        self.norm_wy = self.cum_wy / self.cum_wy.max() # [0,1]
-        # plot the data
-        if plot:
-            plt.figure()
-            for i in range(self.w_y.shape[0]):
-                plt.plot(self.p_x[i], self.w_y[i])
+        for group in self.subgroups:
+            px = []
+            norm_wy = []
+            min_cuts = []
+            max_cuts = []
+            for rep in self.unique_reps:
+                print(group, rep)
+                sub = self.df[(self.df[self.rep] == rep) & (self.df[self.subgroup] == group)]
+                min_cuts.append(sub[self.y].min())
+                max_cuts.append(sub[self.y].max())
+            min_cuts = sorted(min_cuts)
+            max_cuts = sorted(max_cuts)
+            # make linespace of points from highest_min_cut to lowest_max_cut
+            points = list(np.linspace(max(min_cuts), min(max_cuts), num = 128))
+            points = sorted(list(set(min_cuts + points + max_cuts))) 
+            for rep in self.unique_reps:
+                # first point when we catch an empty list caused by uneven rep numbers
+                try:
+                    kde = gaussian_kde(sub[self.y])
+                    kde_points = kde.evaluate(points)
+                    # zero the kde points
+                    kde_points = kde_points - kde_points.min()
+                    zeros_present = list(kde_points)
+                    if print_:
+                        print(f"{zeros_present.count(0)} zeroes present already at index {zeros_present.index(0)}")
+                    # use min and max to make the kde_points outside that dataset = 0
+                    idx_min = min_cuts.index(sub.min())
+                    idx_max = max_cuts.index(sub.max())
+                    if idx_min > 0:
+                        for p in range(idx_min):
+                            kde_points[p] = 0
+                    for idx in range(idx_max - len(max_cuts), 0):
+                        if idx_max - len(max_cuts) != -1:
+                            kde_points[idx+1] = 0
+                    norm_wy.append(kde_points)
+                    px.append(points)
+                except ValueError:
+                    norm_wy.append([])
+                    px.append([])
+            print(f'completed group {group}')
+            px = np.array(px)
+            print(px.shape)
+            # catch the error when there is an empty list added to the dictionary
+            try:
+                norm_wy = np.array(norm_wy)
+            except:
+                length = max([len(e) for e in norm_wy])
+                norm_wy = [a if len(a) > 0 else np.zeros(length) for a in enumerate(norm_wy) ]
+            norm_wy = np.cumsum(norm_wy, axis = 0)
+            try:
+                norm_wy = norm_wy / norm_wy.max() # [0,1]
+            except ValueError:
+                print(norm_wy)
+            self.subgroup_dict[group]['norm_wy'] = norm_wy
+            self.subgroup_dict[group]['px'] = px
+            # work out how to append and then insert into the dictionary
                 
     def check_cutting(self):
         count = 0
@@ -106,6 +122,7 @@ class onionplot:
     
     def plot_stripes(self, total_width = 0.8, linewidth = 2):
         """
+        Only norm_wy and p_x used for this function
         Don't need to evaluate a new KDE, just use the one from the last subarray of w_y
         DONE: add these distances to p_x to get new x values
         DONE: calculate new y values based on these distances
@@ -124,11 +141,11 @@ class onionplot:
         # use last array to plot the outline
         outline_y = np.append(self.p_x[-1], np.flipud(self.p_x[-1]))
         outline_x = np.append(self.norm_wy[-1], np.flipud(self.norm_wy[-1]) * -1) * total_width
-        for i in range(self.w_y.shape[0]):
+        for i in range(self.norm_wy.shape[0]):
             reshaped_x = np.append(self.p_x[i-1], np.flipud(self.p_x[i-1]))
             plt.fill(self.new_wy[i] * total_width, reshaped_x, color = self.colors[i])
         plt.plot(outline_x, outline_y, color = 'Black', linewidth = linewidth)
 
 onion = onionplot(subgroup = 'dose')
-onion.plot_stripes()
-plt.ylabel('Fibre alignment')
+#onion.plot_stripes()
+#plt.ylabel('Fibre alignment')
