5fc89cde8374b1e9cc4c2b4e82112cc78e96016e
Martin <59205886+kynnemall@users.noreply.github.com>
2021-05-31 16:54:35 +0100

    Update setup.py

--- superviolin/setup.py
+++ superviolin/setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
       name = "superviolin",
-      version = 0.13,
+      version = "0.13.1",
       py_modules = ['plot_cli', 'plot'],
       url = "",
       description = "Python CLI to make Violin SuperPlots",
