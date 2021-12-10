ffd22355dc562e30e2c78483dd40eed58e8596f4
Martin <59205886+kynnemall@users.noreply.github.com>
2021-12-10 16:18:36 +0000

    Check numpy version

--- web_app/main.py
+++ web_app/main.py
@@ -25,6 +25,8 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 # </style> """, unsafe_allow_html=True)
 
 st.title("Official Violin SuperPlot Web App")
+import numpy as np
+st.write("Numpy version: {np.__Version__}")
 st.markdown("""
         <p style='text-align: justify'>This web app uses the Superviolin Python package created as part of 
         the editorial <a href='https://www.molbiolcell.org/doi/10.1091/mbc.E21-03-0130'><strong>Violin SuperPlots: Visualizing heterogeneity in large datasets</strong></a> 
