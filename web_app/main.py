e5e111cab4c1e8f3e37cb16f83ca5eeb8b55385d
Martin <59205886+kynnemall@users.noreply.github.com>
2021-12-10 16:34:51 +0000

    Remove numpy version statement

--- web_app/main.py
+++ web_app/main.py
@@ -25,8 +25,6 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 # </style> """, unsafe_allow_html=True)
 
 st.title("Official Violin SuperPlot Web App")
-import numpy as np
-st.write(f"Numpy version: {np.__version__}")
 st.markdown("""
         <p style='text-align: justify'>This web app uses the Superviolin Python package created as part of 
         the editorial <a href='https://www.molbiolcell.org/doi/10.1091/mbc.E21-03-0130'><strong>Violin SuperPlots: Visualizing heterogeneity in large datasets</strong></a> 
